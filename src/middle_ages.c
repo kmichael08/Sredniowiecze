027ac181e1766de8e52da749f0da3bea05b0d912
Michał Kuźba <mk371148@khaki13.mimuw.edu.pl>
2016-05-09 18:59:45 +0200

    Uzycie #define w glownym programie

--- src/middle_ages.c
+++ src/middle_ages.c
@@ -11,6 +11,9 @@
 #include "parse.h"
 #include "engine.h"
 
+#define GAME_FINISHED_CORRECTLY { endGame(1); free(command); return 0; }
+#define GAME_FINISHED_INCORRECTLY {	endGame(0);	free(command); return 42; }
+
 int initsAmount = 0;
 
 int main() {
@@ -23,35 +26,27 @@ int main() {
 		
 		// End of the input.	
 		if (command == NULL) {
-			endGame(koniecGry());	
-			free(command);
-			return 42;
-		}
-				
-		if (!czyPoprawneWejscie) {
-			endGame(0);
-			free(command);
-			return 42;
-		}
+			if(koniecGry())
+				GAME_FINISHED_CORRECTLY
+			else 
+				GAME_FINISHED_INCORRECTLY
+		}		
+		
+		if (!czyPoprawneWejscie) 
+			GAME_FINISHED_INCORRECTLY
 		
 		// First two commands needs to be inits.	
-		if (initsAmount < 2 && command->instruction != INIT) {
-			endGame(0);
-			free(command);
-			return 42;
-		}
+		if (initsAmount < 2 && command->instruction != INIT) 
+			GAME_FINISHED_INCORRECTLY
 			
 		switch(command->instruction) {
 			case INIT : {
 				initsAmount++;
 				
 				if (init(command->boardSize, command->turnNumber, command->player,
-				command->x1, command->y1, command->x2, command->y2)) {
+				command->x1, command->y1, command->x2, command->y2)) 
+					GAME_FINISHED_INCORRECTLY
 					
-					endGame(0);
-					free(command);
-					return 42;
-				}
 				printTopleft();
 			}
 			break;
@@ -61,47 +56,34 @@ int main() {
 			break;
 				
 			case MOVE : 
-				if (move(command->x1, command->y1, command->x2, command->y2)) {
-					endGame(0);
-					free(command);
-					return 42;
-				}
+				if (move(command->x1, command->y1, command->x2, command->y2)) 
+					GAME_FINISHED_INCORRECTLY
+					
 				printTopleft();
 			break;
 				
 			case PRODUCE_KNIGHT :
-				if (produceKnight(command->x1, command->y1, command->x2, command->y2)) {
-					endGame(0);
-					free(command);
-					return 42;
-				}
+				if (produceKnight(command->x1, command->y1, command->x2, command->y2)) 
+					GAME_FINISHED_INCORRECTLY
+					
 				printTopleft();
 			break;
 				
 			case PRODUCE_PEASANT :
-				if (producePeasant(command->x1, command->y1, command->x2, command->y2)) {
-					endGame(0);
-					free(command);
-					return 42;
-				}
+				if (producePeasant(command->x1, command->y1, command->x2, command->y2)) 
+					GAME_FINISHED_INCORRECTLY
 				printTopleft();
 			break;
 			
 			// Wrong command.
 			case WRONG : 
-			{
-				endGame(0);
-				free(command);
-				return 42;
-			}
+			GAME_FINISHED_INCORRECTLY
+			break;
 		}
 		
 		// Game over with a result.
-		if (koniecGry()) {
-			endGame(1);
-			free(command);
-			return 0;
-		}
+		if (koniecGry()) 
+			GAME_FINISHED_CORRECTLY
 			
 		free(command);
 		
